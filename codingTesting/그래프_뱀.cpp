0d4b44ae1737fffaa251ea850dc2e2a79c65c13d
CodeNinja1126 <56903243+CodeNinja1126@users.noreply.github.com>
2021-01-03 00:21:40 +0900

    Update 그래프_뱀.cpp

--- codingTesting/그래프_뱀.cpp
+++ codingTesting/그래프_뱀.cpp
@@ -13,43 +13,46 @@ using namespace std;
 
 deque<pair<int, int>> snake;
 
-bool move(vector<vector<int>> *board, int dir){
+bool move(vector<vector<int>>* board, int dir) {
     pair<int, int> head_pos = snake.front();
     pair<int, int> tail_pos = snake.back();
-    if(dir == 0){
-        if(head_pos.first == board->size() - 1) return false;
-        if((*board)[head_pos.first + 1][head_pos.second] == 1) return false;
-        else if((*board)[head_pos.first + 1][head_pos.second] == 0){
+    if (dir == 0) {
+        if (head_pos.first == board->size() - 1) return false;
+        if ((*board)[head_pos.first + 1][head_pos.second] == 1) return false;
+        else if ((*board)[head_pos.first + 1][head_pos.second] == 0) {
             (*board)[tail_pos.first][tail_pos.second] = 0;
             snake.pop_back();
         }
         (*board)[head_pos.first + 1][head_pos.second] = 1;
         snake.push_front(make_pair(head_pos.first + 1, head_pos.second));
         return true;
-    }else if(dir == 1){
-        if(head_pos.second == board->size() - 1) return false;
-        if((*board)[head_pos.first][head_pos.second + 1] == 1) return false;
-        else if((*board)[head_pos.first][head_pos.second + 1] == 0){
+    }
+    else if (dir == 1) {
+        if (head_pos.second == board->size() - 1) return false;
+        if ((*board)[head_pos.first][head_pos.second + 1] == 1) return false;
+        else if ((*board)[head_pos.first][head_pos.second + 1] == 0) {
             (*board)[tail_pos.first][tail_pos.second] = 0;
             snake.pop_back();
         }
         (*board)[head_pos.first][head_pos.second + 1] = 1;
         snake.push_front(make_pair(head_pos.first, head_pos.second + 1));
         return true;
-    }else if(dir == 2){
-        if(head_pos.first == 0) return false;
-        if((*board)[head_pos.first - 1][head_pos.second] == 1) return false;
-        else if((*board)[head_pos.first - 1][head_pos.second] == 0){
+    }
+    else if (dir == 2) {
+        if (head_pos.first == 0) return false;
+        if ((*board)[head_pos.first - 1][head_pos.second] == 1) return false;
+        else if ((*board)[head_pos.first - 1][head_pos.second] == 0) {
             (*board)[tail_pos.first][tail_pos.second] = 0;
             snake.pop_back();
         }
         (*board)[head_pos.first - 1][head_pos.second] = 1;
         snake.push_front(make_pair(head_pos.first - 1, head_pos.second));
         return true;
-    }else if(dir == 3){
-        if(head_pos.second == 0) return false;
-        if((*board)[head_pos.first][head_pos.second - 1] == 1) return false;
-        else if((*board)[head_pos.first][head_pos.second - 1] == 0){
+    }
+    else if (dir == 3) {
+        if (head_pos.second == 0) return false;
+        if ((*board)[head_pos.first][head_pos.second - 1] == 1) return false;
+        else if ((*board)[head_pos.first][head_pos.second - 1] == 0) {
             (*board)[tail_pos.first][tail_pos.second] = 0;
             snake.pop_back();
         }
@@ -59,54 +62,56 @@ bool move(vector<vector<int>> *board, int dir){
     }
 }
 
-int main(){
+int main() {
     int n;
     cin >> n;
     vector<vector<int>> board(n);
-    
-    for(int i = 0; i < n; i++)
-        for(int j = 0; j < n; j++){
+
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++) {
             board[i].push_back(0);
         }
-    
+
     board[0][0] = 1;
-    snake.push_front(make_pair(0,0));
-    
+    snake.push_front(make_pair(0, 0));
+
     int apple_n;
     cin >> apple_n;
-    for(int i = 0; i < apple_n; i++){
+    for (int i = 0; i < apple_n; i++) {
         int apple_x, apple_y;
         cin >> apple_x;
         cin >> apple_y;
-        board[apple_x - 1][apple_y - 1] = 2;
+        board[apple_y - 1][apple_x - 1] = 2;
     }
-    
+
     int command_n;
     cin >> command_n;
     vector<vector<int>> commands(command_n);
-    for(auto command: commands){
+    for (int i = 0; i < command_n; i++) {
         int temp_time; char temp_com;
         cin >> temp_time;
         cin >> temp_com;
-        command.push_back(temp_time);
-        command.push_back(temp_com);
+        commands[i].push_back(temp_time);
+        commands[i].push_back(temp_com);
     }
-    
+
     int dir = 0, time = 0, command_index = 0;
-    
-    while(move(&board, dir)){
+
+    while (move(&board, dir)) {
         time++;
-        if(command_index < command_n && commands[command_index][0] == time){
-            if('L' == commands[command_index][1]){
+        if (command_index < command_n && commands[command_index][0] == time) {
+            if ('L' == commands[command_index][1]) {
                 dir = (4 + dir - 1) % 4;
-            }else{
+            }
+            else {
                 dir = (dir + 1) % 4;
             }
             command_index++;
         }
     }
-    
+
+    time++;
     cout << time;
-    
+
     return 0;
 }
