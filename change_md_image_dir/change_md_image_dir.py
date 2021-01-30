b0ded87047a8f744cebe792a5d01c77d59d65787
CodeNinja1126 <zkfktm1525@naver.com>
2021-01-30 17:30:17 +0900

    Update change_md_image_dir.py

--- change_md_image_dir/change_md_image_dir.py
+++ change_md_image_dir/change_md_image_dir.py
@@ -2,42 +2,41 @@ import re
 import sys
 import os
 
-if __name__ == '__main__':
-    addr = input('md file name : ')
+addr = input('md file name : ')
 
-    f = None
-    if os.path.isfile(addr):
-        f = open(addr, mode='r', encoding='utf8')
-    else:
-        print('There isn\'t such file')
-        sys.exit(0)
-    # open target md file
+f = None
+if os.path.isfile(addr):
+    f = open(addr, mode='r', encoding='utf8')
+else:
+    print('There isn\'t such file')
+    sys.exit(0)
+# open target md file
 
-    image_addr = input('where is the images? : ')
-    # dir you want to change
+image_addr = input('where is the images? : ')
+# dir you want to change
+
+md_dir = os.path.abspath(addr).split(os.path.sep)
+md_dir = os.path.sep.join(md_dir[:-1]) + os.path.sep
+
+f2 = input('new file name is? : ')
+f2 = open(md_dir+f2, mode='w', encoding='utf8')
+# open new md file
+
+content = 1
+while content:
+    content = f.readline()
+    url = re.findall(r'!\[[\x20-\x7E]+\]',content)
+    # finding embeded image
     
-    md_dir = os.path.abspath(addr).split(os.path.sep)
-    md_dir = os.path.sep.join(md_dir[:-1]) + os.path.sep
-
-    f2 = input('new file name is? : ')
-    f2 = open(md_dir+f2, mode='w', encoding='utf8')
-    # open new md file
-
-    content = 1
-    while content:
-        content = f.readline()
-        url = re.findall(r'!\[[\x20-\x7E]+\]',content)
-        # finding embeded image
+    if url:
+        url = re.findall(r'\([\x20-\x7E]+\)',content)[0]
+        url = url[1:-1]
+        url = url.split(sep='/')
+        content = '/'.join((image_addr, url[-1]))
+        content = f'![{url[-1]}]({content})'
+        # change image directory
         
-        if url:
-            url = re.findall(r'\([\x20-\x7E]+\)',content)[0]
-            url = url[1:-1]
-            url = url.split(sep='/')
-            content = '/'.join((image_addr, url[-1]))
-            content = f'![{url[-1]}]({content})'
-            # change image directory
-            
-        f2.writelines(content)
+    f2.writelines(content)
 
-    f.close()
-    f2.close()
+f.close()
+f2.close()
