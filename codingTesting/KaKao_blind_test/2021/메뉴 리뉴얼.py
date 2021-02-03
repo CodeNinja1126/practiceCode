a55899351864947e0833426137dc1f8105a11a2c
CodeNinja1126 <zkfktm1525@naver.com>
2021-02-03 19:52:34 +0900

    Update 메뉴 리뉴얼.py

--- codingTesting/KaKao_blind_test/2021/메뉴 리뉴얼.py
+++ codingTesting/KaKao_blind_test/2021/메뉴 리뉴얼.py
@@ -5,25 +5,24 @@ https://programmers.co.kr/learn/courses/30/lessons/72411
 '''
 from itertools import combinations
 from collections import Counter
-from collections import defaultdict
 
 def solution(orders, course):
     answer = []
-    comb_dict = defaultdict(lambda: 0)
     
     for n in course:
-        temp_dict = defaultdict(lambda: 0)
+        temp_list = []
         for order in orders:
             temp_order = list(combinations(sorted(order),n))
             for comb in temp_order:
                 comb = ''.join(comb)
-                temp_dict[comb] += 1
+                temp_list.append(comb)
         
-        counts = Counter(temp_dict).most_common()
+        counts = Counter(temp_list).most_common()
         
         answer.extend([v for v, k in counts if k > 1 and k == counts[0][1]])
 
         '''
+        list comprehension을 사용하지 않았을 때
         max_count = 2
         for temp in counts:
             if max_count <= temp[1]:
